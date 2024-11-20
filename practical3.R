344903e3b32ea5003a7f0a75aab2ac700b2d157f
Abdullah Mehmood <am24abp@herts.ac.uk>
2024-11-20 21:34:38 +0000

    first commit

---
+++ practical3.R
@@ -0,0 +1,14 @@
+myvector<-c(1,2,3)
+myvector
+myvector*2
+
+paste("a", "b")
+
+paste(myvector, "ok")
+names<-c('Abdullah', "Azhar","Usama", "Majid", "Bilal")
+paste("Hi to ",names)
+
+goals<-c(0,2,1,3,2)
+mean(goals)
+summary(goals)
+
